efe3fd3e95a1f00c03cd448458d5d38710b7940b
Torsten Bøgh Köster <tbk@thiswayup.de>
2022-10-30 22:16:46 +0100

    mount workshop contents

--- roles/kubernetes/files/k8s-info.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "----------------------------------------------------"
-df -ht ext4
-echo "----------------------------------------------------"
-kubectl get nodes && kubectl describe nodes | grep -A 6 --color=never -e "^Allocated resources"
-echo "----------------------------------------------------"
