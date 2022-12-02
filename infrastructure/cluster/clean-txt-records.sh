dad8baf72b672646294fc35535475bb4de73035b
Torsten Bøgh Köster <tbk@thiswayup.de>
2022-12-02 12:58:02 +0100

    not needed anymore

--- infrastructure/cluster/clean-txt-records.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export DIGITALOCEAN_ACCESS_TOKEN=$(cat secrets.auto.tfvars|grep do_token|awk '{print $3}'|tr -d '"')
-
-acme_txt_records=$(doctl compute domain records list k8s.o12stack.org|grep TXT|grep "_acme-challenge"|awk '{print $1}')
-for record in $acme_txt_records
-do
-        echo "Deleting TXT k8s.o12stack.org ${record} ..."
-        doctl compute domain records delete k8s.o12stack.org ${record} -f
-done
